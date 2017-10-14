0c6fb9bb16c656bbab0cc24aa870a20b6fa4fb74
laggardkernel <laggardkernel@outlook.com>
2017-10-14 21:18:13 +0800

    hello world

---
+++ hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h2>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
