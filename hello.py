1812dc15d4053cb00f2efbe33059985942de781c
laggardkernel <laggardkernel@outlook.com>
2017-10-14 22:22:44 +0800

    jinja template for hello world

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-from flask.ext.script import Manager
+from flask import Flask, render_template
+from flask_script import Manager
 
 app = Flask(__name__)
 manager = Manager(app)
@@ -10,12 +10,12 @@ manager = Manager(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h2>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
