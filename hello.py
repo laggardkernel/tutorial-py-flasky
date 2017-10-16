4a15e1ae1a9eb43bf3a151d4cf8c725208494f07
laggardkernel <laggardkernel@outlook.com>
2017-10-16 21:21:27 +0800

    database models with Flask-SQLAlchemy (05a)

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 from datetime import datetime
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
@@ -9,14 +10,46 @@ from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
 
+base_dir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir,
+    'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+db = SQLAlchemy(app)
 
+
+# ORM models
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    # backref adds role property into User class
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+# Form class for username input
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
@@ -46,5 +79,6 @@ def internal_server_error(e):
 
 
 if __name__ == '__main__':
+    db.create_all()
     # app.run(debug=True)
     manager.run()
