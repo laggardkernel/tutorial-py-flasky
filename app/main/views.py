8b6b749ef6334381891d186e24355a6e0e4518d3
laggardkernel <laggardkernel@outlook.com>
2017-10-21 14:43:31 +0800

    blog posts pagination (11d)

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import hashlib
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request, \
+    current_app
 from flask_login import login_required, current_user
 
 from . import main  # the bluepring
@@ -20,16 +21,26 @@ def index():
             author=current_user._get_current_object())
         db.session.add(post)
         return redirect(url_for('main.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)  # page num from query param
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    return render_template('index.html', form=form, posts=posts,
+        pagination=pagination)
 
 
 @main.route('/user/<username>')
 def user(username):
     """user profile page"""
     user = User.query.filter_by(username=username).first_or_404()
-    posts = user.posts.order_by(Post.timestamp.desc()).all()
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)  # page num from query param
+    pagination = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    return render_template('user.html', user=user, posts=posts,
+        pagination=pagination)
 
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
--- config.py
+++ config.py
@@ -22,6 +22,9 @@ class Config:
     FLASKY_MAIL_SENDER = 'Flasky Admin <%s>' % MAIL_USERNAME
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
+    # pagination
+    FLASKY_POSTS_PER_PAGE = 20
+
     @staticmethod
     def init_app(app):
         pass
