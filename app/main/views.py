80dc0306c72689ba42bed3535757361f9771c193
laggardkernel <laggardkernel@outlook.com>
2017-10-24 12:31:42 +0800

    show followed posts tab in home page (12d)

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@
 
 import hashlib
 from flask import render_template, flash, redirect, url_for, request, \
-    current_app, abort
+    current_app, abort, make_response
 from flask_login import login_required, current_user
 
 from . import main  # the bluepring
@@ -22,12 +22,20 @@ def index():
         db.session.add(post)
         return redirect(url_for('main.index'))
     page = request.args.get('page', 1, type=int)  # page num from query param
-    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+    show_followed = False
+    if current_user.is_authenticated:
+        # show_followed control flag is stored in cookies
+        show_followed = bool(request.cookies.get('show_followed', ''))
+    if show_followed:
+        query = current_user.followed_posts
+    else:
+        query = Post.query
+    pagination = query.order_by(Post.timestamp.desc()).paginate(
         page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
         error_out=False)
     posts = pagination.items
     return render_template('index.html', form=form, posts=posts,
-        pagination=pagination)
+        show_followed=show_followed, pagination=pagination)
 
 
 @main.route('/user/<username>')
@@ -187,3 +195,22 @@ def followed_by(username):
         return render_template('followers.html', user=user,
             title="Followed by", endpoint='main.followed_by',
             pagination=pagination, follows=follows)
+
+
+# set show_followed control flag cookie
+@main.route('/all')
+@login_required
+def show_all():
+    """set show all posts flag before jump to index page"""
+    resp = make_response(redirect(url_for('main.index')))
+    resp.set_cookie('show_followed', '', max_age=30 * 24 * 60 * 60)
+    return resp
+
+
+@main.route('/followed')
+@login_required
+def show_followed():
+    """set show followed posts flag before jump to index page"""
+    resp = make_response(redirect(url_for('main.index')))
+    resp.set_cookie('show_followed', '1', max_age=30 * 24 * 60 * 60)
+    return resp
