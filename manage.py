8f48ed898e3ebabb6f7376b7295763592b1e4c0a
laggardkernel <laggardkernel@outlook.com>
2017-10-28 14:48:50 +0800

    coverage metrics (15a)

--- manage.py
+++ manage.py
@@ -3,6 +3,14 @@
 # Created by laggard on 10/17/17
 
 import os
+
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 from app import create_app, db
 from app.models import User, Follow, Role, Permission, Post, Comment
 from flask_script import Manager, Shell
@@ -27,12 +35,28 @@ manager.add_command('db', MigrateCommand)
 
 # custom manager command for manager shell
 @manager.command
-def test():
+def test(coverage=False):
     """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
     import unittest
     # load test methods from the directory tests
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        import shutil
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        shutil.rmtree(covdir)
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
 
 
 if __name__ == '__main__':
